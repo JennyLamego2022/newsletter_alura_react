7f5b03657414ddd9e493aa82e163a1054373fbf0
JennyLamego2022 <jennylamego@hotmail.com>
2023-06-12 10:40:40 -0300

    feat: criação dos componentes de lista de artigos e artigos

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+
+const Article = ({ title, text, tags, image, alt }) => {
+    return <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center ">
+        <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold">{title}</h3>
+        <div className="grid gap-1">
+            {
+                text.map((content, index) => <p key={index}className="text-alura-200 dark:text-gray-400">{content}</p>)
+            }
+        </div>
+        { image && <img className=' p-4 sm:p-4' src={image}/>}
+        { image && alt && <span className='sr-only'>{alt}</span>}
+    </div>
+};
+
+Article.propTypes = {
+    title: PropTypes.string.isRequired,
+    text: PropTypes.arrayOf(PropTypes.string).isRequired,
+    tags: PropTypes.arrayOf(PropTypes.string),
+    image: PropTypes.string,
+    alt: PropTypes.string,
+  };
+
+export default Article

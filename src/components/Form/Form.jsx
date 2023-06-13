7c889a7feb22f3f18669b4ef39a0639e3b674652
JennyLamego2022 <jennylamego@hotmail.com>
2023-06-13 15:44:42 -0300

    style: Estilização de input e botao

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,7 +12,7 @@ const Form = ({ onSubmit }) => {
     return <form onSubmit={safeSubmit} className=' h-full flex flex-col items-center justify-center gap-10 -mt-20 mx-5'>
         <Input type="text" required placeholder="Insira seu nome..." />
         <Input type="email" required placeholder="Insira seu e-mail..." />
-        <button type="submit" className=' w-full max-w-sm bg-alura-100 dark:bg-dark-200 py-1 px-5 rounded-full text-gray-200 uppercase outline-none cursor-pointer'>Seguir</button>
+        <button type="submit" className=' w-full max-w-sm bg-alura-100 dark:bg-dark-200 py-1 px-5 rounded-full text-gray-200 uppercase outline-none cursor-pointer hover:animate-pulse hover:shadow-md hover:shadow-gray-500 hover:dark:shadow-black'>Seguir</button>
     </form>
 }
 
